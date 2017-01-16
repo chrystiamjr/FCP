1c146405843a7a6dfab0a823d03beca252eabb67
Yusuke <kitabayashiyusuke@gmail.com>
2017-01-16 11:26:29 -0200

    Revert "Commit 1.0"
    This reverts commit b2c8911bc85ec78e369abdd66c30e65e4c3c390b.

--- index.php
+++ index.php
@@ -3,18 +3,6 @@
 
 <?php include_once "includes/header.php"; ?>
 
-<!-- Bootstrap Core CSS -->
-<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- MetisMenu CSS -->
-<link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-
-<!-- Custom CSS -->
-<link href="dist/css/sb-admin-2.css" rel="stylesheet">
-
-<!-- Custom Fonts -->
-<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
 <body>
 
 <div id="wrapper">
